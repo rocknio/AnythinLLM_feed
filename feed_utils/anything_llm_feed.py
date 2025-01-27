a492eebb0024861cf840e73fd53f962effb5f289
rocknio <rocknio@gmail.com>
2025-01-28 00:02:39 +0800

    upload 框架

--- feed_utils/anything_llm_feed.py
+++ feed_utils/anything_llm_feed.py
@@ -3,6 +3,15 @@ def get_workspace_slug(workspace_name):
     return workspace_name.replace(" ", "-")
 
 
-def do_feed_books(txt_file_list):
-    for txt_file in txt_file_list:
-        pass
+def upload_books(txt_file_list):
+    return []
+
+
+def workspace_update_embeddings(work_space_name, txt_file_list):
+    slug = get_workspace_slug(work_space_name)
+    pass
+
+
+def do_feed_books(work_space_name, txt_file_list):
+    file_location_list = upload_books(txt_file_list)
+
