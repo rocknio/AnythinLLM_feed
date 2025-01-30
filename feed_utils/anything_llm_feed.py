bd3b17d59512981ad2e48b338d459018b6869d6a
rocknio <rocknio@gmail.com>
2025-01-30 15:00:40 +0800

    调用anything llm接口

--- feed_utils/anything_llm_feed.py
+++ feed_utils/anything_llm_feed.py
@@ -3,6 +3,7 @@
 import logging
 from pathlib import Path
 from typing import List
+from urllib.parse import quote
 
 import requests
 
@@ -18,14 +19,14 @@ def upload_books(txt_file_list: List[str]) -> List[str]:
         settings = Settings()
         url = f"{settings.AnythingLLM['ApiUrl']}/v1/document/upload"
 
+        files = [
+            ('file', (quote(Path(txt_file).name), open(txt_file, 'rb'), 'text/plain'))
+            for txt_file in txt_file_list
+        ]
+
         headers = {
             "Authorization": f"Bearer {settings.AnythingLLM['ApiKey']}"
         }
-
-        files = [
-            ('file', (Path(txt_file).name, open(txt_file, 'r', encoding='utf-8'), 'text/plain'))
-            for txt_file in txt_file_list
-        ]
 
         response = requests.post(url, headers=headers, files=files)
         if response.status_code == 200:
@@ -63,4 +64,5 @@ def workspace_update_embeddings(file_location_list: List[str]):
 
 def do_feed_books(txt_file_list: List[str]):
     file_location_list = upload_books(txt_file_list)
-    workspace_update_embeddings(file_location_list)
+    if file_location_list:
+        workspace_update_embeddings(file_location_list)
